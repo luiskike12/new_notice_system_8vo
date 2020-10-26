95a82d19d06b62a1c28d4d033e72ed78e994191e
Miguel Alejandro Guevara Mendoza <mike.gm.117@gmail.com>
2020-10-26 12:17:07 -0600

    actualizar alumno accesso a app

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/alumno/registrar',
-        '/alumno/loginApp'
+        '/alumno/loginApp',
+        '/alumno/actualizar'
     ];
 }
