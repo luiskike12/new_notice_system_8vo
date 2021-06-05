6599a713b52c66a3e309727cd99d05c812e2d6c5
LUIS-ENRIQUE\luisenrique <enrique.les.lander@gmail.com>
2021-06-05 12:43:00 -0500

    Agregar las imagenes del nuevo logo NotiSchool

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,41 +1,8 @@
 @extends('auth.contenido')
 
-@section('login')
-
-<!--
-<section >    
-    <div class="baslik">
-        <img src="img/logo_NotiSchool_login.png">
-    </div>
-    <section >
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="envioLogin">
-                <div class="loginbaslik">Login Dashboard</div>
-                <hr style="border: 1px solid #ccc;">
-                <input class="giris" type="text" name="usuario" id="usuario" placeholder="Usuario">
-                <input class="giris" type="password" name="password" id="password" placeholder="Contraseña">
-                <input class="butonlogin" type="submit" name="" value="Iniciar sesión">
-            </div>
-        </form>
-    </section>
-
-</section>
-
-<footer style="margin-top: 40px;">
-    <span style="font-size: 25px; text-align: center; display: block; color: #E6E6E6;
-    ">Panel de acceso a la administración de notificaciones</span>
-    
-    <span style="font-size: 18px; text-align: center; display: block; color: #fff;
-    ">www.notischool.com</span>
-</footer>-->
-
-
-
-
-  
-    <div class="row justify-content-center">
-      <img src="img/logo_NotiSchool_login.png">
+@section('login')  
+    <div class="row justify-content-center" style="margin-bottom: 15px;">
+      <img class="rounded-circle" src="img/logo-NotiSchool-login.jpg">
     </div>
 
     <div class="row justify-content-center">
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="img/logo_NotiSchool.png">
+    <link rel="shortcut icon" href="img/favicon-NotiSchool.png">
     <title>Administracion de notificaciones</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
