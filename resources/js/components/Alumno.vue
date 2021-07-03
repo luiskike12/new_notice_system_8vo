c0dba20b893f7d5713f01c01d25e50397ae68749
LUIS-ENRIQUE\luisenrique <enrique.les.lander@gmail.com>
2021-07-03 15:18:08 -0500

    Mejoras en la (busqueda por filtro) componente Tabla de alumnos Administrador

--- resources/js/components/Alumno.vue
+++ resources/js/components/Alumno.vue
@@ -15,19 +15,19 @@
             <div class="card-body">
                 <div class="form-group">
                     <div class="row scroll-busqueda-filtro">
-                        <div class="col-md-6 scroll-busqueda-filtro-x">
+                        <div v-bind:class="{'col-md-6': col6, 'col-md-10': col10}" class="scroll-busqueda-filtro-x">
                             <div class="input-group">
-                                <select class="form-control col-md-3" v-model="criterio">
+                                <select :class="{'col-md-2': col2, 'col-md-3': col3}" class="form-control" v-model="criterio">
                                     <option value="id_carrera">Carrera</option>
                                     <option value="tipo_modalidad">Modalida</option>
+                                    <option value="grado">Grado</option>
+                                    <option value="turno">Turno</option>
                                     <option value="matricula">Matrícula</option>
                                     <option value="nombre">Nombre</option>
                                     <option value="correo">Correo</option>
-                                    <option value="grado">No. Grado</option>
-                                    <option value="turno">Turno</option>
                                     <option value="condicion">Estado</option>
                                 </select>
-                                <select v-if="criterio==='id_carrera'" class="form-control" v-model="buscar">
+                                <select v-if="criterio==='id_carrera'" class="form-control" v-model="buscar" @click="listarGradosCarrera()">
                                     <option value="" disabled selected>Seleccione una carrera</option>
                                     <option v-for="carrera in arrayCarrera" :key="carrera.id" :value="carrera.id">
                                         {{carrera.nombre}} - {{carrera.tipo_modalidad}}
@@ -50,8 +50,25 @@
                                     <option value="1">Activo</option>
                                     <option value="0">Desactivado</option>
                                 </select>
-                                <input v-else type="text" id="buscar" v-model="buscar" @keyup.enter="listarAlumnos(1, buscar, criterio)" class="form-control" placeholder="Texto a buscar">
-                                <button type="submit" @click="listarAlumnos(1, buscar, criterio)" class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
+                                <input v-else type="text" id="buscar" v-model="buscar" @keyup.enter="listarAlumnos(1, criterio, buscar, buscar2, buscar3)" class="form-control" placeholder="Texto a buscar">
+                                <!-- opciones de filtro enlazadas -->
+                                <!-- grados -->
+                                <select v-if="criterio==='id_carrera' && buscar !== ''" class="form-control col-md-2" v-model="buscar2">
+                                    <option value="" disabled selected>Grado</option>
+                                    <option v-for="grado in arrayGrados" :key="grado.num" :value="grado.num">
+                                        {{grado.num}}º
+                                    </option>
+                                </select>
+                                <!-- turnos -->
+                                <select v-if="criterio==='id_carrera' && buscar !== ''" class="form-control col-md-2" v-model="buscar3">
+                                    <option value="" disabled selected>Turno</option>
+                                    <option value="1">Matutino</option>
+                                    <option value="2">Vespertino</option>
+                                    <option value="3">Nocturno</option>
+                                    <option value="4">Mixto</option>
+                                </select>
+                                <!-- opciones de filtro enlazadas -->
+                                <button type="submit" @click="listarAlumnos(1, criterio, buscar, buscar2, buscar3)" class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
                             </div>
                         </div>
                     </div>
@@ -62,11 +79,11 @@
                             <th class="text-center">Opciones</th>
                             <th class="text-center">Carrera</th>
                             <th class="text-center">Modalidad</th>
+                            <th class="text-center">Grado</th>
+                            <th class="text-center">Turno</th>
                             <th class="text-center">Matrícula</th>
                             <th class="text-center">Nombre</th>
                             <th class="text-center">Correo</th>
-                            <th class="text-center">Grado</th>
-                            <th class="text-center">Turno</th>
                             <th class="text-center">Estado</th>
                         </tr>
                     </thead>
@@ -83,9 +100,6 @@
                             </td>
                             <th v-text="alumno.nombre_carrera"></th>
                             <th v-text="alumno.modalidad_carrera"></th>
-                            <th v-text="alumno.matricula_alumno"></th>
-                            <th v-text="alumno.nombre_alumno"></th>
-                            <th v-text="alumno.correo"></th>
                             <th>{{alumno.grado}}º</th>
                             <th>
                                 <div v-if="alumno.turno==1">Matutino</div>
@@ -93,6 +107,9 @@
                                 <div v-else-if="alumno.turno==3">Nocturno</div>
                                 <div v-else-if="alumno.turno==4">Mixto</div>
                             </th>
+                            <th v-text="alumno.matricula_alumno"></th>
+                            <th v-text="alumno.nombre_alumno"></th>
+                            <th v-text="alumno.correo"></th>
                             <th>
                                 <div v-if="alumno.condicion">
                                     <span class="badge badge-success">Activo</span>
@@ -108,13 +125,13 @@
                 <nav>
                     <ul class="pagination">
                         <li class="page-item" v-if="pagination.current_page > 1">
-                            <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page - 1, buscar, criterio)">Ant</a>
+                            <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page - 1, criterio, buscar, buscar2, buscar3)">Ant</a>
                         </li>
                         <li class="page-item" v-for="page in pagesNumber" :key="page" :class="[page == isActived ? 'active' : '']">
-                            <a class="page-link" href="#" @click.prevent="cambiarPagina(page, buscar, criterio)" v-text="page"></a>
+                            <a class="page-link" href="#" @click.prevent="cambiarPagina(page, criterio, buscar, buscar2, buscar3)" v-text="page"></a>
                         </li>
                         <li class="page-item" v-if="pagination.current_page < pagination.last_page">
-                            <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page + 1, buscar, criterio)">Sig</a>
+                            <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page + 1, criterio, buscar, buscar2, buscar3)">Sig</a>
                         </li>
                     </ul>
                 </nav>
@@ -266,7 +283,14 @@
                 offset : 3,
                 //Busqueda por filtro
                 criterio : 'id_carrera',
-                buscar : ''
+                buscar : '',
+                buscar2 : '',
+                buscar3 : '',
+                //Control del ancho de la columna de busqueda por filtro
+                col2 : false,
+                col3 : true,
+                col6 : true,
+                col10 : false,
             }
         },
         computed : {
@@ -299,20 +323,28 @@
         },
         watch : {
             criterio : function(opcion){
-                if(opcion=='id_carrera')this.buscar = '';
-                if(opcion=='tipo_modalidad')this.buscar = '';
-                if(opcion=='matricula')this.buscar = '';
-                if(opcion=='nombre')this.buscar = '';
-                if(opcion=='correo')this.buscar = '';
-                if(opcion=='grado')this.buscar = '';
-                if(opcion=='turno')this.buscar = '';
-                if(opcion=='condicion')this.buscar = '';
+                /*busqueda_por_filtro(campoBuscar = true, col2 = false, col3 = true, col6 = true, col10 = false)*/
+                if(opcion=='id_carrera')this.busqueda_por_filtro();
+                if(opcion=='tipo_modalidad')this.busqueda_por_filtro();
+                if(opcion=='matricula')this.busqueda_por_filtro();
+                if(opcion=='nombre')this.busqueda_por_filtro();
+                if(opcion=='correo')this.busqueda_por_filtro();
+                if(opcion=='grado')this.busqueda_por_filtro();
+                if(opcion=='turno')this.busqueda_por_filtro();
+                if(opcion=='condicion')this.busqueda_por_filtro();
             }
         },
         methods : {
-            listarAlumnos(page, buscar, criterio){
+            listarAlumnos(page, criterio, buscar, buscar2, buscar3){
                 let me = this;
-                var url = '/alumno?page=' + page + '&buscar=' + buscar + '&criterio=' + criterio;
+                var url = '';
+                var criterio2 = '', criterio3 = '';
+
+                if(buscar2 !== '') criterio2 = 'grado';
+                if(buscar3 !== '') criterio3 = 'turno';
+                
+                url = '/alumno?page='+page+'&criterio='+criterio+'&buscar='+buscar+
+                '&criterio2='+criterio2+'&buscar2='+buscar2+'&criterio3='+criterio3+'&buscar3='+buscar3;
                 //devuelve lo que regresa el controlador de la url /user
                 axios.get(url).then(function (response) {
                     var respuesta = response.data;
@@ -324,19 +356,32 @@
                 })
                 .then(function () {
                     // always executed
-                    me.limpiarTextBuscar();
+                    if(criterio2 === 'grado' || criterio3 === 'turno'){
+                        /*busqueda_por_filtro(campoBuscar, col2, col3, col6, col10)*/
+                        me.busqueda_por_filtro(false,true,false,false,true);
+                    }else{
+                        me.busqueda_por_filtro();
+                    }
                 });
             },
-            cambiarPagina(page, buscar, criterio){//No mover
+            cambiarPagina(page, criterio, buscar, buscar2, buscar3){//No mover
                 let me = this;
                 //actualiza la pagina actual
                 me.pagination.current_page = page;
                 //Envia la peticion para visualizar la data de esa pagina, es para buscar
-                me.listarAlumnos(page, buscar, criterio);
+                me.listarAlumnos(page, criterio, buscar, buscar2, buscar3);
             },
-            limpiarTextBuscar(){
-                $('#buscar').val('');
-                this.buscar = '';
+            busqueda_por_filtro(campoBuscar = true, col2 = false, col3 = true,col6 = true, col10 = false){
+                if(campoBuscar){
+                    $('#buscar').val('');
+                    this.buscar = '';
+                    this.buscar2 = '';
+                    this.buscar3 = '';
+                }
+                this.col2 = col2;
+                this.col3 = col3;
+                this.col6 = col6;
+                this.col10 = col10;
             },
             seleccionarCarrera(){//se puede modificar
                 //obtener el valor de un <select>
@@ -352,6 +397,24 @@
                 .then(function () {
                     // always executed
                 });
+            },
+            listarGradosCarrera(){
+                this.buscar2 = '';
+                this.buscar3 = '';
+                if(typeof(this.buscar)==='number'){
+                    /* busqueda_por_filtro(campoBuscar, col2, col3, col6, col10) */
+                    this.busqueda_por_filtro(false,true,false,false,true);
+                    this.arrayGrados = [];
+                    var numGrados = 0;
+                    for(var i = 0; i < this.arrayCarrera.length; i++){
+                        if(this.buscar == this.arrayCarrera[i]['id']){
+                            numGrados = this.arrayCarrera[i]['num_grados'];
+                            for(var i = 1; i <= numGrados; i++){
+                                this.arrayGrados.push({num: i})
+                            }
+                        }
+                    }
+                }
             },
             seleccionarTurno(id_alumno){//se puede modificar
                 //obtener el valor de un <select>
@@ -395,7 +458,7 @@
                     'turno' : this.turno
                 }).then(function (response){//no modificar
                     me.cerrarModal();
-                    me.listarAlumnos(1, '', 'correo');
+                    me.listarAlumnos(1, '', '', '', '');
                 }).catch(function (error){
                     console.log(error)
                 });
@@ -410,7 +473,7 @@
                     }
                 }).then(function (response){
                     me.cerrarModal();
-                    me.listarAlumnos(1, '', 'correo');
+                    me.listarAlumnos(1, '', '', '', '');
                 }).catch(function (error){
                     console.log(error)
                 });
@@ -522,7 +585,7 @@
             }
         },
         mounted() {//no modificar
-            this.listarAlumnos(1, this.buscar, this.criterio);
+            this.listarAlumnos(1, this.criterio, this.buscar, this.buscar2, this.buscar3);
             this.seleccionarCarrera();
         }
     }
